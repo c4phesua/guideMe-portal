46b37377926cc7bfa4263ea2e7a6877f298f9728
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2021-07-11 19:12:12 +0700

    noti service init

--- lib/services/notificationservice.dart
+++ lib/services/notificationservice.dart
@@ -10,4 +10,32 @@ class NotificationService {
 
   NotificationService._internal();
 
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  Future<void> init() async {
+    final AndroidInitializationSettings initializationSettingsAndroid =
+    AndroidInitializationSettings('app_icon');
+
+    // final IOSInitializationSettings initializationSettingsIOS =
+    // IOSInitializationSettings(
+    //   requestSoundPermission: false,
+    //   requestBadgePermission: false,
+    //   requestAlertPermission: false,
+    //   onDidReceiveLocalNotification: onDidReceiveLocalNotification,
+    // );
+
+    final InitializationSettings initializationSettings =
+    InitializationSettings(
+        android: initializationSettingsAndroid,
+        iOS: null,
+        macOS: null);
+
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: selectNotification);
+  }
+
+
+  Future selectNotification(String payload) async {
+    //Handle notification tapped logic here
+  }
 }
